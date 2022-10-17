f3ddfcd733b66cff1caed5cc3d242e60c341f0d7
Jackeline Lima <jackelinelimap@gmail.com>
2022-10-17 15:07:32 -0300

    organizando endentação

--- flutter_project/lib/responseButton.dart
+++ flutter_project/lib/responseButton.dart
@@ -14,11 +14,16 @@ class ResponseButton extends StatelessWidget {
         onPressed: onTap,
         child: Text(text),
         style: ElevatedButton.styleFrom(
-            textStyle: TextStyle(
-                fontSize: 18, color: Colors.black, fontStyle: FontStyle.italic),
-            side: const BorderSide(color: Colors.black, width: 1),
-            shape: const BeveledRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(5)))),
+          textStyle: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+            fontStyle: FontStyle.italic,
+          ),
+          side: const BorderSide(color: Colors.black, width: 1),
+          shape: const BeveledRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+        ),
       ),
     );
   }
